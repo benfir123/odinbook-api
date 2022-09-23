64d956461b69a9915c244a98702b9fa88b580d8d
Ben Chanapai <ben_chanapai04@hotmail.com>
2022-09-23 15:27:48 +0700

    Add access control method for cors

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ require("./utils/passportConfig");
 
 require("./utils/mongoConfig");
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.options("*", cors());
 app.use(
   cors({
